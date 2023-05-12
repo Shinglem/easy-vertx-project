2b24a7d2cc20471c56155f49e693dc0adaa61ae1
Shinglem <yangmengxin0099@sina.com>
2023-05-12 18:46:18 +0800

    fix

--- pool-log-tracer/build.gradle.kts
+++ pool-log-tracer/build.gradle.kts
@@ -19,7 +19,8 @@ dependencies {
 
     api(group = "io.vertx", name = "vertx-core")
     api(group = "io.vertx", name = "vertx-sql-client")
-
+    //jackson
+    api("com.fasterxml.jackson.core:jackson-databind")
     api("com.github.freva:ascii-table:1.8.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
